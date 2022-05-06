8cb9ff19ec43a08f500f33b6f90deb87e7b82641
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-06 18:36:32 -0300

    challenge 01

---
+++ encriptador/script.js
@@ -0,0 +1,20 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado;
+    mensagem.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    } 
+    return stringEncriptada;
+}
